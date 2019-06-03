2c4ab8b1f272d8f5b705b68fecf88de8e74bc022
Lukas <blassl@student.etzh.ch>
2019-06-03 11:58:07 -0700

    added buggy scripts

---
+++ testprograms/buggy_scripts/Welchs.py
@@ -0,0 +1,51 @@
+import pandas as pd
+from scipy import stats	
+
+# WRANGLING
+
+df = pd.read_excel("../data/iris_data.xlsx")
+print(df)
+
+df.groupby("species")['petal_length'].describe()
+
+setosa = df[(df['species'] == 'Iris-setosa')]
+virginica = df[(df['species'] == 'Iris-virginica')]
+
+
+
+# ANALYSIS
+
+sh1 = stats.shapiro(setosa['petal_length'])
+print(sh1)
+
+sh2 = stats.shapiro(virginica['petal_length'])
+print(sh2)
+
+# this one should work fine
+t1, p1 = stats.ttest_ind(setosa['petal_length'], virginica['petal_length'], equal_var = False)
+
+# but now we compare things we shouldn't => should fail
+t1, p1 = stats.ttest_ind(setosa['petal_length'], virginica['sepal_length'], equal_var = False)
+
+
+def welch_dof(x,y):
+        dof = (x.var()/x.size + y.var()/y.size)**2 / ((x.var()/x.size)**2 / (x.size-1) + (y.var()/y.size)**2 / (y.size-1))
+        print(f"Welch-Satterthwaite Degrees of Freedom= {dof:.4f}")
+        
+welch_dof(setosa['petal_length'], virginica['petal_length'])
+
+def welch_ttest(x, y): 
+    ## Welch-Satterthwaite Degrees of Freedom ##
+    dof = (x.var()/x.size + y.var()/y.size)**2 / ((x.var()/x.size)**2 / (x.size-1) + (y.var()/y.size)**2 / (y.size-1))
+   
+    t, p = stats.ttest_ind(x, y, equal_var = False)
+    
+    print("\n",
+          f"Welch's t-test= {t:.4f}", "\n",
+          f"p-value = {p:.4f}", "\n",
+          f"Welch-Satterthwaite Degrees of Freedom= {dof:.4f}")
+
+# again, this one should work
+welch_ttest(setosa['petal_length'], virginica['petal_length'])
+# but this one should fail
+welch_ttest(setosa['petal_length'], virginica['petal_width'])
---
+++ testprograms/buggy_scripts/lukas_test_1.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  7 16:25:46 2019
+
+@author: Lukas Blass
+"""
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+
+"WRANGLING"
+data = pd.read_csv('../data/timing.csv')
+
+naive = data['naive']
+cache = data['caching']
+fork = data['forking']
+
+avg = (naive + cache + fork) / 3
+
+df =  pd.concat([avg, data['run']], axis=1)
+
+naive_run1 = naive[df['run'] == 1]
+naive_run2 = naive[df['run'] == 2]
+
+fork_run1 = fork[df['run'] == 1]
+fork_run2 = fork[df['run'] == 2]
+
+fork_run_1_2 = fork_run1.append(fork_run2, ignore_index=True)
+naive_run_1_2 = naive_run1.append(naive_run2, ignore_index=True)
+
+# want to have a 'manual' look at data first
+diff = fork_run_1_2 - naive_run_1_2
+print(diff)
+
+
+"ANALYSIS"
+
+# now this should be allowed
+test_result = stats.ttest_ind(fork_run_1_2, naive_run_1_2)
+print(test_result)
+
+# let's do something random
+fork_runs = pd.concat([fork, data['run']], axis=1)
+naive_runs = pd.concat([naive, data['run']], axis=1)
+
+# this should again work since we essentially use the same column twice
+test_result = stats.mannwhitneyu(fork_runs['run'], naive_runs['run'])
+
+# now let's still be random, but also buggy
+long_fork_run = fork_runs['forking'].append(fork_runs['run'])
+long_naive_run = naive_runs['naive'].append(naive_runs['run'])
+
+test_result = stats.mannwhitneyu(long_fork_run, long_naive_run)
+print(test_result)
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
